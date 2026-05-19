5a7e0060deb19e432653c4ffe76e053270ce2b6c
Milk-Cool <coder.tomsk@gmail.com>
2026-05-19 15:07:07 +0300

    oops

--- converter/vite.config.ts
+++ converter/vite.config.ts
@@ -9,5 +9,6 @@ export default defineConfig({
             "Cross-Origin-Opener-Policy": "same-origin",
             "Cross-Origin-Embedder-Policy": "require-corp"
         }
-    }
+    },
+    "base": "/cardtaiko"
 });
